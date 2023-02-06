c2a2c9140dd96a6047db22bf34af7b5818113e35
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2023-02-06 12:20:46 +0100

    Primer dia completado / buscador y inserccion de los elementos escritos con una validacion para que haya minimo 2 caracteres

--- proyecto-gif/src/GifExpertApp.jsx
+++ proyecto-gif/src/GifExpertApp.jsx
@@ -1,7 +1,23 @@
-export const GifExpertApp = ()=>{
-    return(
-        <>
-            <h1>Hola cara cola</h1>
-        </>
-    )
-}
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Goku", "Naruto"]);
+  const onAddCategory = (newCategory) => {
+    setCategories([...categories, newCategory])
+  };
+  return (
+    <>
+      <h1>Hola cara cola</h1>
+      {/* Input */}
+      <AddCategory 
+      // setCategories={ setCategories }
+      onNewCategory={value => onAddCategory(value)}
+      />
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+    </>
+  );
+};
---
+++ proyecto-gif/src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const AddCategory = ({onNewCategory}) => {
+  const [inputValue, setInputValue] = useState("One punch");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+
+    // setCategories( (categories) => [...categories, inputValue])
+    onNewCategory(inputValue.trim())
+    setInputValue('')
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar Gifts"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
